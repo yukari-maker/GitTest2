72ec53f8188d3feee288d76facaa496a3f2ef253
komma <55125997+yukari-maker@users.noreply.github.com>
2020-12-10 18:08:43 +0900

    「10数えるとブザーが鳴ることをループし続ける」コードを追加

--- sound.py
+++ sound.py
@@ -2,13 +2,24 @@ from gpiozero import TonalBuzzer
 from gpiozero.tones import Tone
 from time import sleep
 import time
-b = TonalBuzzer(17)
-sec = 0 #このまま使うとここでカウントリセットされる
 
-for sec in range(10):
-    sec = sec + 1
-    time.sleep(1)
-    print(sec)
-b.play(Tone(600))
-sleep(3)
-b.stop()
+b = TonalBuzzer(17)
+sec = 0
+
+def Buzzer():
+    b.play(Tone(600))
+    sleep(3)
+    b.stop()
+
+def BuzzerTimer():
+    for sec in range(10):
+        sec = sec + 1
+        time.sleep(1)
+        print(sec)
+    Buzzer()
+
+def main_loop():
+    while True:
+        BuzzerTimer()
+if __name__ == '__main__':
+    main_loop()
