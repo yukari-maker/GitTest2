5a9921fae84fccded89f6e4ee76d80eb7b7e88fc
komma <55125997+yukari-maker@users.noreply.github.com>
2020-12-10 18:09:49 +0900

    timerTest.pyがひとまず完成

--- timerTest.py
+++ timerTest.py
@@ -8,25 +8,21 @@ start_time = 0.0
 adc = Pi_hat_adc()
 
 def timer():
-    global start_time#正確かどうか見る
-    start_time = perf_counter()
-    print(start_time)
-
-    sec = 0
-    for sec in range(10):
-        sec = sec + 1
-        time.sleep(1)
+    if adc.get_nchan_adc_raw_data(0) > 500:
+        global start_time#正確かどうか見る
+        start_time = perf_counter()
+        print("started")
+        sec = 0 #このまま使うとここでカウントリセットされる
+        for sec in range(10):
+            sec = sec + 1
+            time.sleep(1)
 
         stop_time = perf_counter()
         end_time =(stop_time-start_time)
         print(end_time)
-
+        print(sec)
 def main_loop():
-    #while True:
-        #print(adc.get_nchan_adc_raw_data(0))
-        sleep(0.5)
-        if adc.get_nchan_adc_raw_data(0) > 1:
-            timer()
-
+    while True:
+        timer()
 if __name__ == '__main__':
     main_loop()
