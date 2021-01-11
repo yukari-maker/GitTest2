edb73dd03ffdcc10e29d02bf5bf42a2198e0106f
komma <55125997+yukari-maker@users.noreply.github.com>
2021-01-11 17:12:20 +0900

    LED_a,bを点灯するコードを追加

--- sound.py
+++ sound.py
@@ -1,13 +1,18 @@
 from gpiozero import TonalBuzzer
 from gpiozero.tones import Tone
+from gpiozero import LED
 from time import sleep
 import time
 
 b = TonalBuzzer(17)
+led_a = LED(22)
+led_b = LED(27)
 sec = 0
 
 def Buzzer():
     b.play(Tone(600))
+    led_a.on()
+    led_b.on()
     sleep(3)
     b.stop()
 
